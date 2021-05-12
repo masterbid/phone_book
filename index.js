664ae2fe3bb9da60cfb61eb6d9ac1b7e333d5805
Bassey Iniabasi <b.iniabasi@gmail.com>
2021-05-13 00:40:21 +0100

    second commit

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ const app = express()
 
 const PORT = process.env.PORT || 7000
 
+const connectionString = process.env.DATABASE_URL
+
 const client = new Client({
-    user: process.env.user,
-    host: process.env.host,
-    database: process.env.database,
-    password: process.env.password,
-    port: process.env.dbport,
+    connectionString
   })
 client.connect().then(() => console.log("DB connected")).catch(err => console.error(err))
 
